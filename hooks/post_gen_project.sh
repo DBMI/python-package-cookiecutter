4ab12800aee1d031d8f3d45ce34b696c045c006c
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-10-03 15:16:34 -0700

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,6 +3,7 @@ cd docs
 sphinx-quickstart
 
 # Initialize as git local repo.
+cd ..
 git init
 git add .
 git commit -m "Initial repo" --no-verify
@@ -12,3 +13,11 @@ gh repo create DBMI/{{cookiecutter.project_name}} --source=. --private
 
 # Push initial code.
 git push -u origin main
+
+# Initialize GitHub pages.
+cd docs/build
+git checkout --orphan gh-pages
+git reset --hard
+git commit --allow-empty -m "Init" --no-verify
+git checkout develop
+git worktree add html gh-pages
