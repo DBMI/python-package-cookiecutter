a7a119bf6b981a29366a9aac555526a1c62fe65a
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-10-03 14:30:37 -0700

    Update run_last_commit.sh
    Don't think we need full path to "anybadge" app.

--- {{cookiecutter.project_slug}}/.github/actions/pre-commit/run_last_commit.sh
+++ {{cookiecutter.project_slug}}/.github/actions/pre-commit/run_last_commit.sh
@@ -1,2 +1,2 @@
 TODAY=$(date +"%B %d, %Y")
-.\\venv\\Scripts\\anybadge -l "last commit" -v "$TODAY" --overwrite --file .\\.github\\badges\\last-commit-badge.svg
+anybadge -l "last commit" -v "$TODAY" --overwrite --file .\\.github\\badges\\last-commit-badge.svg
