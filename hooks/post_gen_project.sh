e14d38539d423f70f11f24eead43d89dabe4ebc2
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-10-03 10:20:29 -0700

    Update post_gen_project.sh
    Add GitHub initialization.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,3 +1,14 @@
-#Initialize documentation.
+# Initialize documentation.
 cd docs
 sphinx-quickstart
+
+# Initialize as git local repo.
+git init
+git add .
+git commit -m "Initial repo" --no-verify
+
+# Create remote repo using GitHub Command Line Interface.
+gh repo create DBMI/{{cookiecutter.project_name}} --source=. --private
+
+# Push initial code.
+git push -u origin main
