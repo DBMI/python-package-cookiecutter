70d00af099aca8523f434ebbe2eff1d1d7d769ab
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-10-05 16:01:27 -0700

    Run sphinx in "--quiet" mode.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,6 @@
 # Initialize documentation.
 cd docs
-sphinx-quickstart
+sphinx-quickstart --quiet -p {{ cookiecutter.project_slug }} -a {{ cookiecutter.author_name }} -v 0.0.1
 
 # Initialize as git local repo.
 echo "Creating local repo."
