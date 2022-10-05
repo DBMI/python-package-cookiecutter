c57387f29e87af7722b725460a913f7e65bd43a3
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-10-05 13:30:27 -0700

    Update post_gen_project.sh
    Test to see if GitHub CLI is installed.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,18 +3,30 @@ cd docs
 sphinx-quickstart
 
 # Initialize as git local repo.
+echo "Creating local repo."
 cd ..
 git init
 git add .
 git commit -m "Initial repo" --no-verify
 
-# Create remote repo using GitHub Command Line Interface.
-gh repo create DBMI/{{cookiecutter.project_name}} --source=. --private
+# Is GitHub Command Line Interface (CLI) installed?
+if gh --version | grep -q 'version'
+then
+  echo "Creating remote repo."
+
+  # Create remote repo using GitHub Command Line Interface.
+  gh repo create DBMI/{{cookiecutter.project_name}} --source=. --private
+else
+  echo "Need to install GitHub Command Line Interface from https://github.com/cli/cli"
+  exit 1
+fi
 
 # Push initial code.
+echo "Pushing initial code."
 git push -u origin main
 
 # Initialize GitHub pages.
+echo "Creating GitHub pages repo."
 cd docs/build
 git checkout --orphan gh-pages
 git reset --hard
