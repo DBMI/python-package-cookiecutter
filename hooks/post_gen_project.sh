c76abda55986b59fa16111917672b03a69e94511
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-10-06 09:57:25 -0700

    Add input variable to read statements.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -13,11 +13,11 @@ git add .
 git commit -m "Initial repo" --no-verify
 git branch -M main
 
-read -p "Ready to create remote repo. Press any key."
+read -p 'Ready to create remote repo. Press any key.' input_key
 
 # Is GitHub Command Line Interface (CLI) installed?
 if gh --version | grep -q 'version' then
-  echo "Form repo name."
+  echo "Forming repo name."
   # Create remote repo using GitHub Command Line Interface.
   repo_name="DBMI/{{ cookiecutter.project_slug }}"
   echo "Creating remote repo ${repo_name}."
@@ -28,17 +28,17 @@ if gh --version | grep -q 'version' then
   git remote add origin ${git_name}
 else
   echo "Need to install GitHub Command Line Interface from https://github.com/cli/cli"
-  read -p "Exiting. Press any key."
+  read -p 'Exiting. Press any key.' input_key
   exit 1
 fi
 
-read -p "Ready to push code to remote repo. Press any key."
+read -p 'Ready to push code to remote repo. Press any key.' input_key
 
 # Push initial code.
 echo "Pushing initial code."
 git push -u origin main
 
-read -p "Ready to create GitHub pages. Press any key."
+read -p 'Ready to create GitHub pages. Press any key.' input_key
 
 # Initialize GitHub pages.
 echo "Creating GitHub pages repo."
@@ -49,4 +49,4 @@ git commit --allow-empty -m "Init" --no-verify
 git checkout develop
 git worktree add html gh-pages
 
-read -p "Project setup complete. Press any key."
+read -p 'Project setup complete. Press any key.' input_key
