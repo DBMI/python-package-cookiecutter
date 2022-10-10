649101926c6aab0b8b62b61a7c2c6717d58b30e8
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-10-10 14:15:00 -0700

    Remove diagnostics

--- extensions/string_filters_extension.py
+++ extensions/string_filters_extension.py
@@ -9,14 +9,10 @@ def email_from_author(author):
     :return: email
     ex {{ "Alpha B. Charlie" | email_from_author }} --> "abcharlie"
     """
-    print(dir(author))
-    """
     author_despaced = author.replace(' ', '')
     author_depunctuated = author_despaced.replace('.', '')
     email_address = re.sub(r'[a-z]*([A-Z])', '\\1', author_depunctuated)
     return email_address.lower()
-    """
-    return 'nobody'
 
 
 def upper_case_first_letter(value):
