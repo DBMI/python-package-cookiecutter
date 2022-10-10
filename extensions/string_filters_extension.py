87736bcb7e60911cafb9552c42cfad0b9646be6d
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-10-10 15:25:43 -0700

    Add optional email server.

--- extensions/string_filters_extension.py
+++ extensions/string_filters_extension.py
@@ -2,17 +2,19 @@ from jinja2.ext import Extension
 import re
 
 
-def email_from_author(author):
+def email_from_author(author, email_server = "ucsd.edu"):
     """
     Converts an author's name into an email address.
     :param author: author's name
+           email_server: like "yahoo.com"
     :return: email
-    ex {{ "Alpha B. Charlie" | email_from_author }} --> "abcharlie"
+    ex {{ "Alpha B. Charlie" | email_from_author }} --> "abcharlie@ucsd.edu"
+    ex {{ "Alpha B. Charlie" | email_from_author("gmail.com" }} --> "abcharlie@gmail.com"
     """
     author_despaced = author.replace(' ', '')
     author_depunctuated = author_despaced.replace('.', '')
     email_address = re.sub(r'[a-z]*([A-Z])', '\\1', author_depunctuated)
-    return email_address.lower()
+    return email_address.lower() + '@' + email_server
 
 
 def upper_case_first_letter(value):
